8d4613815471d9b98621fd8ec14e129a068bc5b5
Giovdev <gio.orellanac@duocuc.cl>
2024-05-29 09:22:55 -0400

    Cree mi diccionario del ejercicio

---
+++ ejemplo_git.py
@@ -0,0 +1,9 @@
+#Ejercicio diccionario de hoy: Usuario:
+diccionario = {
+    "Identificador": 1,
+    "nombre": "Marriott" ,
+    "dirección": "Avenida Bernardo O'higgins 1123",
+    "funcionando": True,
+    "valor habitacion": 1000000
+
+}
